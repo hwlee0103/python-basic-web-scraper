0db3fa9f7188d8bdf75551ff4cf8cccaf24a27f5
hwlee0103 <sdiverHyeWon0103@gmail.com>
2021-03-27 21:42:15 +0900

    elif and or
    if-else, elif, and, or, <, ==, >, and etc.

--- ch1/chapter1_2.py
+++ ch1/chapter1_2.py
@@ -15,3 +15,19 @@ def check_num_plus2(a, b):
 
 print(check_num_plus2(12, "10"))
 print(check_num_plus2(12, 1.2))
+
+#if else and or
+def age_check(age):
+    print(f"you are {age}")
+    if age < 18:
+        print("you cant drink")
+    elif age == 18:
+        print("you are new to this!")
+    elif age > 20 and age < 25:
+        print("you are still kind of young")
+    else:
+        print("enjoy your drink")
+
+age_check(18)
+age_check(23)
+age_check(29)
