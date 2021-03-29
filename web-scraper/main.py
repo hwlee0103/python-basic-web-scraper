43d5510def27cfb582a81dbb1d689e90134cea76
hwlee0103 <sdiverHyeWon0103@gmail.com>
2021-03-29 20:47:17 +0900

    scraping
    scraping indeed site

--- web-scraper/main.py
+++ web-scraper/main.py
@@ -9,4 +9,26 @@ indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
 #result: <Response [200]>
 
 indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
-print(indeed_soup)
+#print(indeed_soup)
+
+#pagination = indeed_soup.find("div", {"class":"pagination"})
+pagination = indeed_soup.find("ul", {"class":"pagination-list"})
+#print(pagination)
+
+pages = pagination.find_all('a')
+#print(pages)
+spans = []
+for page in pages:
+  #print(page.find("span"))
+  spans.append(page.find("span"))
+  #spans.append(page.find("b"))
+
+#print(spans)
+print(spans[-1])
+print(spans[:-1])
+pans = spans[:-1]
+
+#-1 means from last item to first item
+#print(spans[-1])
+#0~5
+#print(spans[0:5])
