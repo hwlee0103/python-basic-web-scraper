e6f50ff5f509a9690289b322b1811603545bbfc2
hwlee0103 <sdiverHyeWon0103@gmail.com>
2021-04-05 23:13:20 +0900

    scrape pages and info box
    extract page info and job info box from stackoverflow

--- web-scraper/soverflow.py
+++ web-scraper/soverflow.py
@@ -9,9 +9,26 @@ def get_last_page():
     soup = BeautifulSoup(result.text, "html.parser")
     #print(soup)
     pages = soup.find("div", {"class":"s-pagination"}).find_all("a")
+    #print(pages[-1])
+    #print(pages[0:-2])
+    #pages = pages[-2]
     #print(pages)
+    last_page = pages[-2].get_text(strip=True)
+    return last_page
 
+def extract_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        #print(page + 1)
+        result = requests.get(f"{URL}&pg={page+1}")
+        #print(result.status_code)
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class":"-job"})
+        for result in results:
+            print(result["data-jobid"])
 
 def get_jobs():
     last_page = get_last_page()
+    #print(last_page)
+    jobs = extract_jobs(last_page)
     return []
