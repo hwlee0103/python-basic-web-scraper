5cff3b336f4cae712c5814fc98d1e7b313eacba8
hwlee0103 <sdiverHyeWon0103@gmail.com>
2021-04-16 23:04:48 +0900

    Dynamic urls and render templates
    Dynamic urls and render templates; using render_template and flask

--- ex-web-scraper/main.py
+++ ex-web-scraper/main.py
@@ -1,16 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("SuperScrapper")
 
 @app.route("/")
 def home():
-    return "Hello! Welcome to mi casa!"
+    #return "Hello! Welcome to mi casa!"
+    #return "<h1>Job Search</h1><form><input placeholder='What job do you want?' required /><button>Search</button>"
+    return render_template("potato.html")
 
-@app.route("/contact")
+#@app.route("/contact")
+@app.route("/<username>")
 #def contact():
 #it is okay to use different name on function
-def potato(): 
-    return "Contact me!"
+def potato(username): 
+    #return "Contact me!"
+    return f"Hello {username} how are you doing"
 
 app.run(host="0.0.0.0")
 
